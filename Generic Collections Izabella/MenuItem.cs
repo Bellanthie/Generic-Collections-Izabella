4ea457b584f9694a195765e5c04e4f6a59afd9fe
Izabella <bellanthie@gmail.com>
2025-11-13 13:34:37 +0100

    Add project files.

---
+++ Generic Collections Izabella/MenuItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Collections_Izabella
+{
+    internal class MenuItem
+    {
+
+        // Predefined properties for menuItem class
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public MenuItem(int id, string name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Price:C}";
+        }
+
+
+    }
+}
---
+++ Generic Collections Izabella/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Collections_Izabella
+{
+    internal class Order
+    {
+        // Private fields
+        static int orderIdCounter = 1;
+        private int _orderId;
+        private List<MenuItem> _orderItems;
+        private int _tableNumber;
+
+        public Order(List<MenuItem> orderItems, int tableNumber)
+        {
+            // Auto IDs
+            _orderId = orderIdCounter;
+            orderIdCounter++;
+            _orderItems = orderItems;
+            _tableNumber = tableNumber;
+        }
+
+    }
+}
